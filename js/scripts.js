7f8f75f13d3ecd5946699d23f744d232fde842f7
Cienszki <wilq.wdz@gmail.com>
2018-02-22 19:34:36 +0100

    Change errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var list = document.getElementById('list'),
 	add = document.getElementById('addElem');
 
-add.addEventListener('click', function addElements() {
+add.addEventListener('click', function addElements {
 	var element = document.createElement('li');	
 	var allElementsSoFar = document.getElementsByTagName('li');
 	element.innerHTML = 'item ' + allElementsSoFar.length;
